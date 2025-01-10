a07eddf9bcb9b42451e827c273b083a34a320b77
Joshua Hammer <jph33@outlook.com>
2025-01-10 15:01:29 -0500

    it almost works

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -3,10 +3,10 @@
   lua =
     #lua
     ''
-      require("nvim-treesitter.configs").setup({
-        sync_install = true,
-        auto_install = true,
-        highlight = {enable = true},
-      })
+      -- require("nvim-treesitter.configs").setup({
+      --   sync_install = true,
+      --   auto_install = true,
+      --   highlight = {enable = true},
+      -- })
     '';
 }

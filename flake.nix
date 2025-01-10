07398ee64bf36749768e3e2179688e8958e5bddb
Joshua Hammer <jph33@outlook.com>
2025-01-10 10:25:41 -0500

    batman

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "mvim: malleus vim config powered by nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      perSystem = {pkgs, ...}: let
+        modules = import ./config;
+        mvim = pkgs.neovim.override {
+          viAlias = true;
+          configure = {
+            packages.myVimPackage.start =  modules.packages;
+            customRC = ''
+              lua << EOF
+              ${modules.lua}
+              EOF
+            '';
+          };
+        };
+      in {
+        packages.default = mvim;
+      };
+    };
+}

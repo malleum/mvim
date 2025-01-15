d6137a7c524c0e7b54e015934450e8ff372ee7c4
joshua hammer <jph33@outlook.com>
2025-01-15 17:19:22 -0500

    treesitter fix

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -3,10 +3,8 @@
   lua =
     #lua
     ''
-      -- require("nvim-treesitter.configs").setup({
-      --   sync_install = true,
-      --   auto_install = true,
-      --   highlight = {enable = true},
-      -- })
+      require("nvim-treesitter.configs").setup({
+        highlight = {enable = true},
+      })
     '';
 }
